1f32566e1ee98f6942c5370757e435ac274527c3
Alok <alokk48200@gmail.com>
2025-12-12 19:18:59 +0530

    changes done for this main branch

--- Temop/src/main/java/logger/ConsoleObserver.java
+++ Temop/src/main/java/logger/ConsoleObserver.java
@@ -4,5 +4,6 @@ public class ConsoleObserver implements LogObserver{
     @Override
     public void notify(String message) {
         System.out.println("this messages i sread in console observer");
+        System.out.println("Alok ");
     }
 }
